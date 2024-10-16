724f6b504645650a6141c489cf9c40ecbe84a02f
Claude_Delcroix <166008956+Yurri5code@users.noreply.github.com>
2024-10-16 15:02:31 +0300

    ajout de la fonction pangrams

--- string/string.c
+++ string/string.c
@@ -90,6 +90,31 @@ unsigned Pos(string1 Subs,string1 s){
     }
 }
 
+char* pangrams(char* s) {
+    if(strlen(s) < 26){
+        return "not pangram";
+    }
+    char* example = {"abcdefghijklmnopqrstuvwxyz"};
+    int verif = 0;
+    for(int i = 0;i < 26;i++){
+        for(int j = 0;j < strlen(s);j++){
+            char c = s[j];
+            if(s[j]>= 'A' && s[j]<='Z'){
+                c = tolower(s[j]);
+            }
+            if(example[i] == c){
+                verif++;
+            }
+        }
+        if(verif == 0){
+            return "not pangram";
+        }
+        verif = 0;
+    }
+    
+    return "pangram";
+}
+
 void DoneStr(string1 s){
     if(s != NULL){
         free(s);
