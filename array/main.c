fe07d214e0c681d04856b75b769ddfc749750d0e
Claude_Delcroix <166008956+Yurri5code@users.noreply.github.com>
2024-12-14 01:41:40 +0300

    Update main.c

--- array/main.c
+++ array/main.c
@@ -43,7 +43,82 @@ void testIntersection2() {
     free(result);
 }
 
+void testIsperfetSquart() {
+    printf("entrez une valeur nous allons verifier si elle est un perfect square\n");
+    printf("x =  ");
+    int x = 0;
+    scanf("%d",&x);
+
+    if(isPerfectSquare(x)) {
+        printf("%d is the perfect square\n",x);
+    }else {
+        printf("is not a perfect square\n");
+    }
+}
+
+void testQuickSort() {
+    int arr[] = {23,12,34,56,43,23,12,34,67,786,75,455,6545,54};
+    size_t size = sizeof(arr)/sizeof(arr[0]);
+    printArray(arr,size);
+
+    quickSort(arr,0,size - 1);
+    printArray(arr,size);
+}
+
+void testQuickSort1() {
+    int arr[] = {4,5,3,7,2};
+    size_t size = sizeof(arr)/sizeof(arr[0]);
+    printArray(arr,size);
+    int ok = 0;
+    int *ar = quickSort1(size,arr,&ok);
+    printArray(ar,ok);
+}
+
+void testShellSort() {
+    int scores[] = {9,6,5,8,0,7,4,3,1,2};
+    int length = sizeof(scores)/sizeof(scores[0]);
+    shellShort(scores,length);
+    for(int i = 0;i < length;i++){
+        printf("%d ",*(scores + i));
+    }
+}
+
+void testThirdMax() {
+    int arr[] = {1,1,2};
+    size_t len = sizeof(arr)/sizeof(arr[0]);
+    const int third = thirdMax(arr,len);
+
+    printf("The third max value %d \n",third);
+}
+
+void testCutTheSticks() {
+    int sticks[] = {5,4,4,2,2,8};
+    int size = sizeof(sticks)/sizeof(sticks[0]),returnSize = 0;
+    int* result = cutTheSticks(size,sticks,&returnSize);
+
+    printArray(result,returnSize);
+}
+
+void testCountingort() {
+    int arr[] = {19,10,12,10,24,25,22};
+    const size_t size = sizeof(arr)/sizeof(arr[0]);
+
+    int ok = 0;
+
+    int* array = countingSort(size,arr,&ok);
+
+    printArray(array,ok);
+    printArray(arr,size);
+
+    free(array);
+}
+
 int main(void) {
-    testIntersection2();
+    unsigned int result = difference_of_squares(5);
+
+    printf("difference = %u\n",result);
+    printf("square of sum = %u\n",square_of_sum(5));
+    printf("sum of square = %u\n",sum_of_squares(5));
+
     return 0;
 }
